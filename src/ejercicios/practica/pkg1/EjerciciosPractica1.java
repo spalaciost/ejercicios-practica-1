f050e0fd3025e2dd127a804a5c20002c0fede064
Estudiante <Estudiante@FI454402-31.unal.loc>
2016-08-09 15:00:49 -0500

    Ejercicios de la practica 1

---
+++ src/ejercicios/practica/pkg1/EjerciciosPractica1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios.practica.pkg1;
+
+/**
+ *
+ * @author Santiago Palacios
+ * @since 09/08/16
+ * @version 1
+ */
+public class EjerciciosPractica1 {
+
+    public static void main (String[] args) {
+        int x=3;
+      while(x>0){
+        if (x>2) {
+            System.out.print("a"); 
+        }
+        x=x-1;
+          System.out.print("-");
+        if (x==2){
+              System.out.print("b c");
+        }
+        if(x==1){
+            System.out.print("d");
+            x=x-1;
+        }
+        }
+    }
+}
+   
